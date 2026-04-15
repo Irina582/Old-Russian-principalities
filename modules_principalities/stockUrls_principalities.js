f39473ecc720f92d844b051edbfa2675a0aee4b5
Ирина Зеленева <zirik06@gmail.com>
2026-04-15 19:19:39 +0300

    изменения

--- modules_principalities/stockUrls_principalities.js
+++ modules_principalities/stockUrls_principalities.js
@@ -1,26 +1,26 @@
 class StockUrls {
     constructor() {
-        this.baseUrl = 'http://localhost:5500';
+        this.baseUrl = 'http://localhost:3000';
     }
 
     getStocks() {
-        return `${this.baseUrl}/stocks`;
+        return `${this.baseUrl}/stocks_principalities`;
     }
 
     getStockById(id) {
-        return `${this.baseUrl}/stocks/${id}`;
+        return `${this.baseUrl}/stocks_principalities/${id}`;
     }
 
     createStock() {
-        return `${this.baseUrl}/stocks`;
+        return `${this.baseUrl}/stocks_principalities`;
     }
 
     removeStockById() {
-        return `${this.baseUrl}/stocks/${id}`;
+        return `${this.baseUrl}/stocks_principalities/${id}`;
     }
 
     updateStockById() {
-        return `${this.baseUrl}/stocks/${id}`;
+        return `${this.baseUrl}/stocks_principalities/${id}`;
     }
 }
 
